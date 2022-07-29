8bd576b2424ebd0f93dd13a02ad3b4797bc90663
Joseph Olamiye <captainjoe550@gmail.com>
2022-07-29 17:06:07 +0100

    adding javasript functionalities

---
+++ index.js
@@ -0,0 +1,18 @@
+let num = document.getElementById("num")
+let count = 0
+
+function clickit(){ 
+    count = count + 1
+    num.innerText = count
+}
+
+function clickit(){
+    count = count + 2
+    num.innerText = count
+
+}
+
+function clickit(){
+    count = count + 3
+    num.innerText = count
+ }
