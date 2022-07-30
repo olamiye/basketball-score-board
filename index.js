ac1141d0b22a5c3681e5ecd4833c3eed94a07c58
Joseph Olamiye <captainjoe550@gmail.com>
2022-07-30 21:20:30 +0100

    added all functionalities

--- index.js
+++ index.js
@@ -2,19 +2,40 @@ let num = document.getElementById("num")
 let count = 0
 
 function clickit(){ 
-    count = count + 1
+    count += 1
     num.innerText = count
     
 }
 
  function clickit2(){
-     count = count + 2
+    count += 2
     num.innerText = count
 
 }
 
 function clickit3(){
-    count = count + 3
+    count +=3
     num.innerText = count
+    
+}
+
+//guest section
+let num2 = document.getElementById("num2")
+let countEl = 0
+function clickit4(){ 
+    countEl +=1
+    num2.innerText = countEl
+    
+}
+
+ function clickit5(){
+    countEl +=2
+    num2.innerText = countEl
+
+}
+
+function clickit6(){
+    countEl +=3
+    num2.innerText = countEl
     
 }
